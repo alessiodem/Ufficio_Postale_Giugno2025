88f13d4ddda0e72471ec15747855ac615586bc00
alessio <alessio.demarco587@edu.unito.it>
2025-06-07 16:49:26 +0200

    eliminato print ticket

--- lib/utils.c
+++ lib/utils.c
@@ -25,13 +25,3 @@ Service get_random_service() {
 ServiceType get_random_service_type() {
     return (ServiceType)(rand() % NUM_SERVIZI);
 }
-
-void print_ticket_debug( Ticket ticket) {
-
-    printf("===== Ticket Debug Info =====\n");
-    printf("Numero: %d\n", ticket.ticket_index);
-    printf("Tipo di Servizio: %d\n", ticket.service_type);
-    printf("Tempo Stimato: %d secondi\n", ticket.actual_deliver_time);
-    printf("Stato: %s\n", ticket.is_done ? "Completato" : "In Attesa");
-    printf("=============================\n");
-}
